c70d4fc0a66efde8c49e05275d9de82fa81eaf1f
Anders Eie <strykejernene@gmail.com>
2013-03-08 16:49:41 +0100

    Comments!

--- x2-gameproject/x2-gameproject/x2-gameproject/GameState.cs
+++ x2-gameproject/x2-gameproject/x2-gameproject/GameState.cs
@@ -8,6 +8,7 @@ using System.IO;
 
 namespace X2Game {
 	abstract class GameState {
+		/** Is this state supposed to be an overlay for other states? */
 		public readonly bool isOverlay = false;
 
 		protected GameState(bool overlay)
@@ -15,9 +16,24 @@ namespace X2Game {
 			isOverlay = overlay;
 		}
 
+		/**
+		 *	Update the current GameState
+		 *
+		 *	Returns true if it is finished and ready for a call to getNextState()
+		 */
 		public abstract bool Update();
+
+		/** Draw the current GameState */
 		public abstract void Draw(SpriteBatch spriteBatch);
+
+		/** Send input to the GameState */
 		public abstract void Input(KeyboardState keyboard);
+
+		/**
+		 * Get new state which this state spawned.
+		 * 
+		 * Returns null if this state is finished
+		 */
 		public abstract GameState getNextState();
 	}
 }
