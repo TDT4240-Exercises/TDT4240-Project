a2761ae2a14e50810eefbd1f11b6f222418f9b79
Anders Eie <strykejernene@gmail.com>
2013-03-08 16:54:40 +0100

    Fixed spawnParticle case

--- x2-gameproject/x2-gameproject/x2-gameproject/MainMenuState.cs
+++ x2-gameproject/x2-gameproject/x2-gameproject/MainMenuState.cs
@@ -38,12 +38,12 @@ namespace X2Game {
 			if (keyboard.IsKeyDown (Keys.Escape)) {
 				buttonPressed = true;
 			} else if (keyboard.IsKeyDown (Keys.P) && !spawnParticle) {
-				ParticleEngine.spawnParticle (new Vector2 (rand.Next (800), rand.Next (600)), ResourceManager.getParticleTemplate ("fireball.xml"));
+				ParticleEngine.SpawnParticle (new Vector2 (rand.Next (800), rand.Next (600)), ResourceManager.getParticleTemplate ("fireball.xml"));
 				spawnParticle = true;
 			} else if (keyboard.IsKeyUp (Keys.P) && spawnParticle)
 				spawnParticle = false;
 			else if (keyboard.IsKeyDown (Keys.S))
-				ParticleEngine.spawnParticle (new Vector2 (rand.Next (800), rand.Next (600)), ResourceManager.getParticleTemplate ("fireball.xml"));
+				ParticleEngine.SpawnParticle (new Vector2 (rand.Next (800), rand.Next (600)), ResourceManager.getParticleTemplate ("fireball.xml"));
 		}
 		
 		public override GameState getNextState ()
