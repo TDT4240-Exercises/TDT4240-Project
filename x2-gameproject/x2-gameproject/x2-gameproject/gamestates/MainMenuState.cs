12dc780ffe959e2e4d9ae0a1f2a963b9e53963ea
Peter A. Voigt <mousetrapp@gmail.com>
2013-04-03 17:29:45 +0200

    Skrev en komentar for å teste commit.

--- x2-gameproject/x2-gameproject/x2-gameproject/gamestates/MainMenuState.cs
+++ x2-gameproject/x2-gameproject/x2-gameproject/gamestates/MainMenuState.cs
@@ -8,6 +8,7 @@ namespace X2Game {
     class MainMenuState : GameState {
         private bool spawnParticle;
         private Random rand;
+        // Test peter push
         
         public MainMenuState()
         {
